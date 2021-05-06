c0b956fd90123bb35ce0757cb33adf8e0faeb0a9
kartingist <awjon94@gmail.com>
2021-05-06 15:27:26 +0300

    настроил запуск тестов, добавил тесты регистрации и логина

---
+++ pages/base_page.py
@@ -0,0 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
+from .locators import MainPageLocators
+from selenium.webdriver.common.by import By
+
+import math
+from selenium.common.exceptions import NoAlertPresentException
+
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
---
+++ test_ui.py
@@ -0,0 +1,33 @@
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import time
+login='heatcliff5'
+
+def test_registration(browser):
+    link = "http://users.bugred.ru/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    login_page.registration(login)
+    page.open()
+    main_page=MainPage(browser, browser.current_url)
+    main_page.check_registration(login)
+
+
+def test_login(browser):
+    link = "http://users.bugred.ru/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    login_page.login(login)
+    main_page = MainPage(browser, browser.current_url)
+    main_page.check_login(login)
+
+
+
+
+
